64859da7f59b7046bca23329eb8348703ce6bee5
Sebastian Smela <Sebastian.Smela123@windowslive.com>
2021-10-27 14:02:20 +0200

    additional configs/ index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,38 @@
-console.log("initial commit");
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const config = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const app = express();
+
+app.use(
+  express.urlencoded({
+    limit: "100mb",
+    extended: true,
+  })
+);
+
+app.use(
+  express.json({
+    limit: "100mb",
+    extended: true,
+  })
+);
+
+app.use(cors());
+
+mongoose
+  .connect(process.env.CONNECTION_URL, config)
+  .then(() =>
+    app.listen(process.env.PORT, () =>
+      console.log(`server is running on port ${process.env.PORT}`)
+    )
+  )
+  .catch((error) => console.log(error.message));
