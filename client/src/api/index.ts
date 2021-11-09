ac1eedfa3ad6846b20504d8f439d7f9a22f1c9e2
Sebastian Smela <Sebastian.Smela123@windowslive.com>
2021-11-09 14:30:05 +0100

    backend heroku

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -5,10 +5,10 @@ const productsUrl: string = "https://fakestoreapi.com/products";
 export const fetchProducts = () => axios.get(productsUrl);
 export const fetchProduct = (id: string) => axios.get(`${productsUrl}/${id}`);
 
-const loginUrl: string = "http://localhost:5000/signin";
+const loginUrl: string = "https://backend-for-shop.herokuapp.com/signin";
 
 export const signInUser = (user: object) => axios.post(loginUrl, user);
 
-const registerUrl: string = "http://localhost:5000/register";
+const registerUrl: string = "https://backend-for-shop.herokuapp.com/register";
 
 export const registerUser = (user: object) => axios.post(registerUrl, user);
